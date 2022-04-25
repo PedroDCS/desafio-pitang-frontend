9a80c5da4c562b51b80a23ff7727a6967c8f35eb
Pedro Daniel Camargos Soares <pedrodanielcamargos.pd@gmail.com>
2022-04-25 10:35:27 +0000

    Develop merge to main

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,24 @@
+import ButtonHome from '../../components/Button/ButtonHome';
 import styles from './HomePage.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 function HomePage() {
+    const navigate = useNavigate()
+
+    const navigateToConsultas = () => {
+        navigate('/consultas',{state:{dayProp: new Date()}})
+    }
+    const navigateTomarcarConsultas = () => {
+        navigate('/marcarconsulta')
+    }
+    const navigateToRegister = () => {
+        navigate('/registro')
+    }
     return (<div className={styles.home_container}>
-        <p className={styles.hometitle}>Agendamento de Vacinas COVID-19</p>
+        <h1>Agendamento de Vacinas COVID-19</h1>
+        <ButtonHome onclick={navigateToConsultas} text="Consultar Agendamentos" />
+        <ButtonHome onclick={navigateTomarcarConsultas} text="Agendar horarios" />
+        <ButtonHome onclick={navigateToRegister} text="criar conta" />
     </div>
     );
 }
